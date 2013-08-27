2fc462d9f55fc498eb14a05c2d64920dc60c78d7
bmeisele <bmeisele@redhat.com>
2013-08-27 15:35:16 -0400

    removed discounts example

--- drools-reference-implementation/examples/discounts/src/main/java/com/rhc/discounts/domain/IDiscount.java
+++
@@ -1,8 +0,0 @@
-package com.rhc.discounts.domain;
-
-public interface IDiscount {
-	public String getType();
-	public float getValue(float cost);
-	public int getMax();
-	public void setMax(int max);
-}
